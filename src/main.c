aa0b8facbacedd0a10f9baee216c3462cf8ab67b
KyanPepper <kyankks1@gmail.com>
2024-04-17 01:06:13 -0700

    Scheduler config

--- src/main.c
+++ src/main.c
@@ -1,3 +1,17 @@
-int main(){
+#include "scheduler.c"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+
+int main(int argc, char *argv[])
+{
+    uint32_t total_num_of_process;               // Read from the file -- number of process to create
+    _process process_list[total_num_of_process]; // Creates a container for all processes
+    // Other variables
+
+
+    // Write code for your shiny scheduler
+
     return 0;
-}
+} 
